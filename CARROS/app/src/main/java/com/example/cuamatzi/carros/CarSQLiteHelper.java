7596f7c45fae73f5827ee50a169d2cec092690b8
Cristian Reyes Cuamatzi <cuamatzicrc@gmail.com>
2019-10-04 00:04:07 -0500

    SQLite basico con ejemplo de autos
    Ejemplo de SQLite en Android

---
+++ CARROS/app/src/main/java/com/example/cuamatzi/carros/CarSQLiteHelper.java
@@ -0,0 +1,39 @@
+package com.example.cuamatzi.carros;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class CarSQLiteHelper extends SQLiteOpenHelper
+{
+    //Creamos la tabla
+    private String sqlCreate = "CREATE TABLE Cars (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+            "marca TEXT," +
+            "modelo TEXT)";
+
+    private String sqlDelete = "DROP TABLE IF EXISTS Cars";
+
+    public CarSQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        sqLiteDatabase.execSQL(sqlCreate);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
+    {
+        //Si existe una versión previa hay que eliminarla
+        sqLiteDatabase.execSQL(sqlDelete);
+        //Creamos una nueva versión
+        sqLiteDatabase.execSQL(sqlCreate);
+    }
+}
